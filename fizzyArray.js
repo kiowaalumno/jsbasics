926c911eb03f49e068fbd084df9e5e2a185f6eb4
Kiowa Alumno <kiowaalumno@gmail.com>
2018-12-10 11:09:26 -0600

    fixed?

---
+++ fizzyArray.js
@@ -0,0 +1,14 @@
+
+
+//takes numbers and put them in an array
+
+function numarray(n) {
+    resultArr = [n];
+    for (i = 0; i < n; i++) {
+        resultArr[i] = i ;
+    }
+    console.log(resultArr);
+}
+//this starts my method
+numarray(14);
+numarray(4);
---
+++ main.js
@@ -0,0 +1,6 @@
+// Given two int values, return their sum. Unless the two values are the same,
+// then return double their sum.
+    for (i = 0; i <= 100; i++); {
+    if (i % 2 !== 0);
+    console.log(i);
+    }
